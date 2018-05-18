bf6a5ad03cb9abff06239ab5cf84857d423a6063
Marco Fugaro <marco.fugaro@gmail.com>
2018-05-18 17:54:08 +0200

    📦  Upgrade dependencies and accurapp

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,17 @@
 const path = require('path')
-const buildWebpackConfig = require('webpack-preset-accurapp')
-const { NamedModulesPlugin } = require('webpack')
-const { setOutput, addPlugins, customConfig } = require('webpack-blocks')
+const { buildWebpackConfig } = require('webpack-preset-accurapp')
+const { env, setOutput, customConfig } = require('webpack-blocks')
 const npmModulesExternals = require('webpack-node-externals')
 
-function setExternals(externals) {
-  return customConfig({ externals })
-}
-
-const appConfig = buildWebpackConfig()
-
-const libConfig = buildWebpackConfig([
-  setOutput({
-    ...appConfig.output,
-    path: path.resolve('./lib'),
-    filename: 'react-components.js',
-    libraryTarget: 'commonjs2',
-  }),
-  setExternals([npmModulesExternals()]),
-  addPlugins([new NamedModulesPlugin()]),
+module.exports = buildWebpackConfig([
+  env('production', [
+    setOutput({
+      path: path.resolve('./lib'),
+      filename: 'react-components.js',
+      libraryTarget: 'commonjs2',
+    }),
+    customConfig({
+      externals: [npmModulesExternals()],
+    }),
+  ]),
 ])
-
-module.exports = libConfig
